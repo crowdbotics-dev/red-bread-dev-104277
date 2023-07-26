7b975291ba095f91318e9d181b82a1f2875abdbd
Crowdbotics <engineering@crowdbotics.com>
2023-07-26 08:22:02 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-bread-dev-104277",
+  url: "https://red-bread-dev-104277.botics.co",
+  api: "https://red-bread-dev-104277.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
